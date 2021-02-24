23241f3006d5ec20341929313afff3af2089efe8
eden-edwards <73649434+eden-edwards@users.noreply.github.com>
2021-02-24 20:49:49 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const about = document.getElementById("about")
+
+about.onclick = () => {
+    location.href = "./about.html"
+}
+
+const contact = document.getElementById("contact")
+
+contact.onclick = () => {
+    location.href = "./contact.html"
+}
+
+const menu = document.getElementById("menu")
+
+menu.onclick = () => {
+    location.href = "./menu.html"
+}
+
+
